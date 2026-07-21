b21a3df94e4407aa659412fd3f4112ec79e6d013
samotsaristore <carlito.sano888@gmail.com>
2026-07-21 19:25:54 +0800

    Create funko.js

---
+++ products/funko.js
@@ -0,0 +1,53 @@
+const funkoProducts = [
+
+{
+name:"X-23",
+image:"images/funko/x23.jpg",
+price:"₱599",
+direct:"₱349",
+shopee:"#",
+lazada:"#",
+facebook:"x23"
+},
+
+{
+name:"Old Man Logan",
+image:"images/funko/oldmanlogan.jpg",
+price:"₱599",
+direct:"₱349",
+shopee:"#",
+lazada:"#",
+facebook:"logan"
+},
+
+{
+name:"Wolverine",
+image:"images/funko/wolverine.jpg",
+price:"₱599",
+direct:"₱349",
+shopee:"#",
+lazada:"#",
+facebook:"wolverine"
+},
+
+{
+name:"Queen Funko Pop",
+image:"images/funko/queen.jpg",
+price:"₱588",
+direct:"₱348",
+shopee:"#",
+lazada:"#",
+facebook:"queen"
+},
+
+{
+name:"Dracula Funko Pop",
+image:"images/funko/dracula.jpg",
+price:"₱589",
+direct:"₱349",
+shopee:"#",
+lazada:"#",
+facebook:"dracula"
+}
+
+];
