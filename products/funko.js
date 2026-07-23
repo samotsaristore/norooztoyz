d774f77175c6c153effa3c59c59b995e80cd285e
samotsaristore <carlito.sano888@gmail.com>
2026-07-24 00:26:16 +0800

    Update - funko_js_backup

--- products/funko.js
+++ products/funko.js
@@ -1,1 +1,71 @@
-c
+const funkoProducts = [
+
+    {
+        id: "x23",
+
+        name: "X-23",
+
+        image: "images/x23.jpg",
+
+        price: "₱599",
+
+        directBuy: "₱349",
+
+        status: "AVAILABLE NOW",
+
+        shopee: "https://shopee.ph",
+
+        lazada: "https://lazada.com.ph",
+
+        facebook: "https://m.me",
+
+        modal: "x23"
+    },
+
+
+    {
+        id: "logan",
+
+        name: "Old Man Logan",
+
+        image: "images/oldmanlogan.jpg",
+
+        price: "₱599",
+
+        directBuy: "₱349",
+
+        status: "AVAILABLE NOW",
+
+        shopee: "https://shopee.ph",
+
+        lazada: "https://lazada.com.ph",
+
+        facebook: "https://m.me",
+
+        modal: "logan"
+    },
+
+
+    {
+        id: "wolverine",
+
+        name: "Wolverine",
+
+        image: "images/wolverine.jpg",
+
+        price: "₱599",
+
+        directBuy: "₱349",
+
+        status: "AVAILABLE NOW",
+
+        shopee: "https://shopee.ph",
+
+        lazada: "https://lazada.com.ph",
+
+        facebook: "https://m.me",
+
+        modal: "wolverine"
+    }
+
+];
