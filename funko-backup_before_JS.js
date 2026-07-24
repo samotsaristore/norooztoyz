99d1e6e3f0f78b0937fc9d3bc0ee7b22c99dfe2e
samotsaristore <carlito.sano888@gmail.com>
2026-07-24 10:44:42 +0800

    Improve Funko Pop responsive product layout
    Updated Funko Pop product grid layout for better desktop viewing.
    Changed desktop display to show 3 products per row while keeping the mobile browsing experience.

--- funko-backup_before_JS.js
+++ funko-backup_before_JS.js
@@ -2,71 +2,70 @@ const funkoProducts = [
 
     {
         id: "x23",
+
         name: "X-23",
+
         image: "images/x23.jpg",
+
         price: "₱599",
+
         directBuy: "₱349",
+
         status: "AVAILABLE NOW",
+
         shopee: "https://shopee.ph",
+
         lazada: "https://lazada.com.ph",
+
         facebook: "https://m.me",
+
         modal: "x23"
     },
 
 
     {
         id: "logan",
+
         name: "Old Man Logan",
+
         image: "images/oldmanlogan.jpg",
+
         price: "₱599",
+
         directBuy: "₱349",
+
         status: "AVAILABLE NOW",
+
         shopee: "https://shopee.ph",
+
         lazada: "https://lazada.com.ph",
+
         facebook: "https://m.me",
+
         modal: "logan"
     },
 
 
     {
         id: "wolverine",
+
         name: "Wolverine",
+
         image: "images/wolverine.jpg",
+
         price: "₱599",
+
         directBuy: "₱349",
+
         status: "AVAILABLE NOW",
+
         shopee: "https://shopee.ph",
+
         lazada: "https://lazada.com.ph",
+
         facebook: "https://m.me",
+
         modal: "wolverine"
-    },
-
-
-    {
-        id: "dracula",
-        name: "Dracula",
-        image: "images/dracula.jpg",
-        price: "₱599",
-        directBuy: "₱349",
-        status: "AVAILABLE NOW",
-        shopee: "https://shopee.ph",
-        lazada: "https://lazada.com.ph",
-        facebook: "https://m.me",
-        modal: "dracula"
-    },
-
-
-    {
-        id: "freddie",
-        name: "Freddie Mercury",
-        image: "images/freddie.jpg",
-        price: "₱599",
-        directBuy: "₱349",
-        status: "AVAILABLE NOW",
-        shopee: "https://shopee.ph",
-        lazada: "https://lazada.com.ph",
-        facebook: "https://m.me",
-        modal: "freddie"
     }
 
 ];
